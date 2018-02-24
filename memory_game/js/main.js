7d74bb1cf071ccabc4040af1eea33ff10c8718a5
Ticktock <smu@ticktock.local>
2018-02-24 11:45:25 +0800

    added game logic to javascript file

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,24 @@
 console.log("Up and running!");
 
-var cardOne="queen";
-var cardTwo="king";
-var cardThree="queen";
-var cardFour="king";
-
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+//create game logic
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []; //create an empty array to hold cards in play.
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[0]);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2 &&
+	cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match");
+	} else {
+		alert("Sorry, try again");
+	}
+
+
