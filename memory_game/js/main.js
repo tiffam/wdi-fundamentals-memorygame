1c2302b96e72134c8cdc8163cf89feaba57cc3d2
Ticktock <smu@ticktock.local>
2018-02-24 14:44:07 +0800

    added flipcard functions and cleaned up code

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,28 @@
-console.log("Up and running!");
-
-console.log("User flipped " + cardOne);
 
 //create game logic
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = []; //create an empty array to hold cards in play.
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0]);
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1]);
+// create checkformatch function
 
-if (cardsInPlay.length === 2 &&
-	cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match");
-	} else {
-		alert("Sorry, try again");
-	}
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+}
+
+// create flipcard function
+
+var flipCard = function(cardID) {
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch();
+}
 
 
+
+flipCard(0);
+flipCard(2);
