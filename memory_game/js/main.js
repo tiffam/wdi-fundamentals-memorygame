51a4b96d56da89491cb47cc2235574c345f46f0c
Ticktock <smu@ticktock.local>
2018-02-25 19:13:23 +0800

    created game logic - replaced string with objects for cards

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,28 +1,55 @@
 
 //create game logic
+var cardsArray = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{	
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+]
 
-var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = []; //create an empty array to hold cards in play.
 
 // create checkformatch function
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
-} else {
-console.log("Sorry, try again.");
+	alert("You found a match!");
+} else if (cardsInPlay.length<2) {
+	alert("Flip one more card");
+}
+else {
+	alert("Sorry, try again.");
 }
 }
 
 // create flipcard function
 
+var cardID;
+
 var flipCard = function(cardID) {
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped " + cardsArray[cardID].rank);
+	console.log(cardsArray[cardID].cardImage);
+ 	console.log(cardsArray[cardID].suit);
+	cardsInPlay.push(cardsArray[cardID].rank);
 	checkForMatch();
 }
 
 
-
 flipCard(0);
-flipCard(2);
+flipCard(1);
